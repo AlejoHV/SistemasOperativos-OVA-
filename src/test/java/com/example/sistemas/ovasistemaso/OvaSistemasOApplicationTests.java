d38a9d68a27a976cd62ec79cd0588ec67ba79074
Alejandro Hernandez <jahv0125@gmail.com>
2025-05-23 01:34:29 -0500

    Creación proyecto

---
+++ src/test/java/com/example/sistemas/ovasistemaso/OvaSistemasOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemas.ovasistemaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvaSistemasOApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
